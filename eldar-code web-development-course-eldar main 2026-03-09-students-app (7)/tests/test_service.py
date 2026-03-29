3c6455d087db6bef894888bd5df5770a41081304
Linoy <linoyh3@gmail.com>
2026-03-30 02:53:29 +0300

    Update Students Tests added

--- eldar-code web-development-course-eldar main 2026-03-09-students-app (7)/tests/test_service.py
+++ eldar-code web-development-course-eldar main 2026-03-09-students-app (7)/tests/test_service.py
@@ -90,4 +90,56 @@ class TestService(TestCase):
         self.assertRaises(ServiceError, service.add_student, {'name': '', 'age': 11, 'id': 101})
         mock_add_student.assert_not_called()
 
+    # ============== Update Student - Positive Tests =============== #
+
+    @patch("app.service.db.update_student")
+    def test_update_student_age_in_range(self, mock_update_student: Mock):
+        mock_update_student.return_value = {'name': 'Mock Student', 'age': 25, 'id': 101}
+        students =[
+        {'name': 'Danny Parry', 'age': 18, 'id': 101},
+        {'name': 'John Nick', 'age': 26, 'id': 89},
+        {'name': 'Matty King', 'age': 62, 'id': 3},
+        {'name': 'Lona Dean', 'age': 120, 'id': 69}
+        ]
+        for student in students:
+            result = service.update_student(student)
+            self.assertEqual({'name': 'Mock Student', 'age': 25, 'id': 101}, result)
+        self.assertEqual(4, mock_update_student.call_count)
+
+    @patch("app.service.db.update_student")
+    def test_Student_must_have_a_name_with_minimum_length(self, mock_update_student: Mock):
+        mock_update_student.return_value = {'name': 'Mock Student', 'age': 25, 'id': 101}
+        student = {'name': 'M', 'age': 25, 'id': 101}
+        result = service.update_student(student)
+        self.assertEqual({'name': 'Mock Student', 'age': 25, 'id': 101}, result)
+        mock_update_student.assert_called_once_with(student)
+
+    # ============== Update Student - Negative Tests =============== #
+
+    @patch("app.service.db.update_student")
+    def test_age_is_not_in_the_range(self, mock_update_student: Mock):
+        mock_update_student.side_effect = ServiceError("Student age is illegal")
+        students = [
+            {'name': 'Mock Student', 'age': 4, 'id': 101},
+            {'name': 'Mock Student', 'age': 145, 'id': 101},
+            {'name': 'Mock Student', 'age': -2, 'id': 101}
+        ]
+        for student in students:
+            self.assertRaises(ServiceError, service.update_student, student)
+        mock_update_student.assert_not_called()
+
+    @patch("app.service.db.update_student")
+    def test_name_with_no_minimum_length_of_1_letter(self, mock_update_student:Mock):
+        mock_update_student.side_effect = ServiceError("Student name is illegal")
+        self.assertRaises(ServiceError, service.update_student, {'name': '', 'age': 40, 'id': 101})
+        mock_update_student.assert_not_called()
+
+
+
+
+
+
+
+
+
 
